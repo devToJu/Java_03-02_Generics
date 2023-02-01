11be13710f3b75b047b33558e0bf48c5218513e3
devtoju <msg.toju@outlook.de>
2023-02-01 16:29:48 +0100

    iterate through a list contains only user-objects

--- src/com/devToJu/Generics/Main.java
+++ src/com/devToJu/Generics/Main.java
@@ -10,15 +10,14 @@ public class Main {
         var admins = new UpToTenElementsOfSameType<Admin>();
         admins.add(new Admin("x", "1"));
         admins.add(new Admin("y", "2"));
-        printUsers(admins);
+        // Doesn't compile -> NOT OK: Admin is-a User (it's a Subclass)
+        // printUsers(admins);
 
-        var numbers = new UpToTenElementsOfSameType<Integer>();
-        numbers.add(10);
-        numbers.add(23);
-        printUsers(numbers);
+        // Doesn't compile -> OK: Integer isn't a User
+        // printUsers(new UpToTenElementsOfSameType<Integer>());
     }
 
-    private static void printUsers(UpToTenElementsOfSameType tenItems) {
+    private static void printUsers(UpToTenElementsOfSameType<User> tenItems) {
         for (int i = 0; i < tenItems.getCount(); i++) {
             System.out.println(tenItems.get(i));
         }
