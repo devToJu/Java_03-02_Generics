926c9473b3b041c1c5f78984bc9475783966d24a
devtoju <msg.toju@outlook.de>
2023-02-01 15:45:07 +0100

    init use of wildcard

--- src/com/devToJu/Generics/Main.java
+++ src/com/devToJu/Generics/Main.java
@@ -2,51 +2,6 @@ package com.devToJu.Generics;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("The user who ist online the most is " + getUserWithMaxOnlineTime());
-        System.out.println("The biggest value is " + getMaxValue());
-        System.out.println("String comparing returns: " + getMaxString());
 
-        System.out.println("------------------------------------------------------");
-
-        print("String Key", "String value");
-        print(1, "String value");
-        print(2, new User("noname", "1234"));
-
-        System.out.println("------------------------------------------------------");
-
-        print(new KeyValuePair<>(33, "Text..."));
-    }
-
-    private static User getUserWithMaxOnlineTime() {
-        var userA = new User("schnuffy23", "bad_decision");
-        var userB = new User("test0815", "password1234");
-
-        return max(userA, userB);
-    }
-
-    private static Double getMaxValue() {
-        var a = Double.valueOf(Math.random() * 10.4);
-        var b = Double.valueOf(Math.random() * 13.7);
-
-        return max(a, b);
-    }
-
-    private static String getMaxString() {
-        var textA = "Hello? Hello!";
-        var textB = "What's going on?";
-
-        return max(textA, textB);
-    }
-
-    private static <T extends Comparable<T>> T max(T first, T second) {
-        return first.compareTo(second) >= 0 ? first : second;
-    }
-
-    private static <K, V> void print(K key, V value) {
-        System.out.println("key: " + key + " | value: " + value);
-    }
-
-    private static <K, V> void print(KeyValuePair<K, V> keyValuePair) {
-        print(keyValuePair.getKey(), keyValuePair.getValue());
     }
 }
